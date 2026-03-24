608709237a686f6bd59d03db38ecfc0be8966cda
root <root@HaiderPC.localdomain>
2026-03-24 12:53:43 +0100

    made server.py more effective for 3rd assignment

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def index():
 def sets():
     template = open("templates/sets.html").read()
     rows = ""
-
+    result = []
     start_time = perf_counter()
     conn = psycopg.connect(**DB_CONFIG)
     try:
@@ -34,13 +34,12 @@ def sets():
             for row in cur.fetchall():
                 html_safe_id = html.escape(row[0])
                 html_safe_name = html.escape(row[1])
-                existing_rows = rows
-                rows = existing_rows + f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n'
+                result.append(f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n')
         print(f"Time to render all sets: {perf_counter() - start_time}")
     finally:
         conn.close()
 
-    page_html = template.replace("{ROWS}", rows)
+    page_html = template.replace("{ROWS}", "".join(result))
     return Response(page_html, content_type="text/html")
 
 
